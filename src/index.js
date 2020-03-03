5e3b0f9610730311af14c399fc8de73496021dfd
rxhl <maniaxecorp@gmail.com>
2020-03-02 22:01:22 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+// utils
+const { MONGODB, PORT } = require('./config');
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+// Every query/subscription must have a resolver
+const resolvers = {
+  Query: {
+    sayHi: () => 'Hello world'
+  }
+};
+
+// Set up Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Connect to MongoDB Atlas &&
+// Start the Apollo server (uses express under the hood)
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB connected!');
+    return server.listen({ port: PORT });
+  })
+  .then(res => {
+    console.log(`Server running at ${res.url}...`);
+  });
