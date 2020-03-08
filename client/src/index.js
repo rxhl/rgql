83d39f6cfc0a774362f79c4609603bdac27c2fd0
rxhl <maniaxecorp@gmail.com>
2020-03-08 11:58:29 -0400

    Setup client

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ApolloPovider from './ApolloProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(ApolloPovider, document.getElementById('root'));
